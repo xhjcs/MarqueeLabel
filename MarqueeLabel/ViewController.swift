641ff7f136086c55d89e6c0f069f28c2d2b8aada
Heikki <heikki@Heikkis-Mac-mini.local>
2017-12-01 22:05:29 +0800

    适配 ios8.0

--- MarqueeLabel/ViewController.swift
+++ MarqueeLabel/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         ml.text = "只是重写了get方法，我们称这个属性为“计算性”属性，也就是只读属性"
         ml.play()
         self.view.addSubview(ml)
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(10)) {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(5)) {
             ml.text = "你希望上拉的时候图片显示还是不显示？是 gif 还是文字？"
         }
     }
